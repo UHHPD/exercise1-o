8e67359bbf41c613e11e4a2b407e0b00e8cec8e4
Viacheslav Kosterin <77215394+slavheatcliff7@users.noreply.github.com>
2021-11-10 16:29:46 +0000

    sreda2

--- likelihood.cc
+++ likelihood.cc
@@ -33,13 +33,23 @@ int main () {
 itog = 1;
 double itog2;
 double itog1;
+double itog3;
+itog3=1;
+double itog4;
 std::vector<int> daten;
 std::ifstream fin( "datensumme.txt" );
 std::ofstream fout( "likelihood.txt" );
 std::ofstream fout1( "nll.txt" );
- while ( !fin.eof() ) {
+std::ofstream fout2( "deltanll.txt" );
+  while ( !fin.eof() ) {
        fin >> zahl;
        daten.push_back(zahl); 
+  }
+ for(unsigned int k = 0 ; k < (daten.size()-1) ; ++k) { 
+      itog3 = itog3 * prob (daten, 3.11538, k);
+
+   
+      //std::cout << k << "   mu  = "<< mu << << itog << std::endl;
  }
 mu1 = 0.1;   
   while (mu1 < 6) { 
@@ -53,11 +63,14 @@ mu1 = 0.1;
    fout <<  mu1 << " " << itog << std::endl;
    itog1 = log (itog); 
    itog2 = -2*itog1;
+    
+
+   itog4 = itog2 - (-2*(log (itog3)));
    fout1 <<  mu1 << " " << itog2 << std::endl;
-   
+   fout2 << mu1 << " " << itog4 << std::endl;
    itog = 1;
    mu1=mu1+0.1;
   } 
   
-
+ std::cout << itog3 << std::endl;
 }
